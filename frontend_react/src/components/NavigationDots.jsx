66cbb450d4e11b8bfe78c35584d7724d831af1a7
Luisito Canlas <luisitocanlas@gmail.com>
2023-08-03 22:15:21 -0700

    adding frontend

---
+++ frontend_react/src/components/NavigationDots.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const NavigationDots = ({ active }) => {
+  return (
+    <div className='app__navigation'>
+        
+        {/* Array of items for the navbar */}
+        {['home', 'work', 'skills', 'contact'].map((item, index) => (
+
+            // Create each list item as an anchor
+            <a 
+                href={`#${item}`}
+                key = {item + index}
+                className='app__navigation-dot'
+                style={active === item ? { backgroundColor: '#313BAC' } : { }}
+           />
+            
+        ))}
+
+    </div>
+  )
+}
+
+export default NavigationDots
